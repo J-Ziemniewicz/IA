a74c0520ae8c19f12c819ca58c9de6ee399938e7
Jan <fokzterrier@gmail.com>
2019-04-01 22:24:38 +0200

    Final version

--- Lab4_add/aeg_crawler.js
+++ Lab4_add/aeg_crawler.js
@@ -1,39 +1,44 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
+const sortJsonArray = require('sort-json-array');
 const infoScraper = require('./infoScraper');
 const url = 'https://gunfire.com/pl/menu/karabiny-i-karabinki-elektryczne-17201.html';
 
 rp(url)
     .then(function (htmlString) {
         //success!
-        // console.log(htmlString);
-        // console.log($('div[id="component_projector_longdescription"]', htmlString).text());
         const aegUrls = [];
-        const aegList = [];
+        
         var x = $('div[class="product_wrapper col-md-4 col-xs-6"] a[class="product-name"]', htmlString).length;
         for (let i = 0; i < x; i++) {
             aegUrls.push($('div[class="product_wrapper col-md-4 col-xs-6"] a[class="product-name"]', htmlString)[i].attribs.href);
         };
-        for (let i =0;i<aegUrls.length;i++){
-            let {model,fps,price} = infoScraper(url);
-            aegList.push([model,fps,price]);
-        };
-        console.log(aegUrls);
-        console.log(aegList);
-        // return aegList;
         
-        // return Promise.all(
-        //     aegUrls.map(function (url) {
-        //         var {model,fps,price} = infoScraper(url);
-        //         return {model,fps,price};
-        //         // return {model,fps,price} = infoScraper(url);
-        //         // return infoScraper(url);
-        //     })
-        // );
+        return Promise.all(
+            aegUrls.map(function (url) {
+                // var {model,fps,price} = infoScraper(url);
+                // return {model,fps,price};
+                // return {model,fps,price} = infoScraper(url);
+                return infoScraper(url);
+            })
+        );
     })
-    // .then(function (aeg) {
-    //     console.log(aeg);
-    // })
+    .then(function (aeg) {
+        for (let i =aeg.length-1;i>0;i--){
+            if(aeg[i].model ==""||aeg[i].fps==""){
+                aeg.splice(i,1);
+            }
+        }
+        for (let i= 0;i<aeg.length;i++){
+            aeg[i].value = aeg[i].price/aeg[i].fps;
+        }
+        for (let i =aeg.length-1;i>0;i--){
+            if(isNaN(aeg[i].value)){
+                aeg.splice(i,1);
+            }
+        }
+        console.log(sortJsonArray(aeg,'value'));
+    })
     .catch(function (err) {
         //handle error
         console.log(err);
