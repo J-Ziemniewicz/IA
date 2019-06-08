13edbda2bf2da9fb58a65ac541d68c85ec5776da
Fokzterrier <fokzterrier@gmail.com>
2019-06-08 23:21:50 +0200

    almost finished

--- Lab10/react-front/text-editor/src/App.js
+++ Lab10/react-front/text-editor/src/App.js
@@ -4,83 +4,83 @@ import './App.css';
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.text="";
+    this.text = "";
 
     this.state = {
       isLogged: false,
       login: "",
+      text: "",
     };
 
-    this.showText=this.showText.bind(this);
-    this.showLogin=this.showLogin.bind(this);
-    this.handleChange=this.handleChange.bind(this);
-    this.textContainer=this.textContainer.bind(this);
+    this.showText = this.showText.bind(this);
+    this.showLogin = this.showLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
-    
-  showText(login){
+
+  showText(login) {
     this.setState({
-      isLogged:true,
-      login:login,
+      isLogged: true,
+      login: login,
     })
   }
 
-  showLogin(){
+  showLogin() {
     this.setState({
-      isLogged:false,
+      isLogged: false,
     })
-  }
-  
-  textContainer(){
-    console.log("Jestem w części tekstowej");
-    return(
-      <div></div>
-    );
-    
   }
 
   handleChange(e) {
 
     if (e.target.value !== "") {
       this.setState({
-        login:e.target.value
+        login: e.target.value
       });
     }
-    
   }
 
-
-  render (){
-    var login= (
+  render() {
+    var login = (
       <div>
-      <div className="Log-menu">
-        Menu logowania
+        <div className="Log-menu">
+          Menu logowania
       </div>
-      <div className="Log-input">
-      <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
-      <button className="Log-btn" onClick={()=>this.showText(login)}>Loguj</button>
-      </div>
+        <div className="Log-input">
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
+          <button className="Log-btn" onClick={() => this.showText(login)}>Loguj</button>
+        </div>
       </div>
     );
 
-    return(
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className = "App-name">
-          Notatnik
-        </p>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <div className="Container">
-        {this.state.isLogged ? this.textContainer() : login}
+    var text = (
+      <div>
+        <div className="Log-menu">
+          Notatka
+          </div>
+        <div>
+          <input type="text" className="input" onChange={this.handleChange} placeholder="Search..." />
+        </div>
       </div>
-    </div>
-  )
-  
-}
+    );
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="App-name">
+            Notatnik
+        </p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <div className="Container">
+          {this.state.isLogged ? text : login}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
